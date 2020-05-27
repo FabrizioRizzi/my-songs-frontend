997900d3d30af2fdba8b352d26bdac538dac35ac
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2020-05-27 19:39:46 +0200

    Added login form prototype

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import {
   Box,
   Button,
-  Grommet,
   Form,
   FormField,
   MaskedInput,
   TextInput
 } from "grommet";
 
+const defaultValue = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
 
   const history = useHistory();
+  const [value, setValue] = useState(defaultValue);
 
-  const clicca = () => {
-
-    const data = { email: "fabrizio@fabriziorizzi.it", password: "mysongs9" };
-
-    axios.post('https://my-songs-backend.herokuapp.com/authentication/login/', data)
-      .then(function (response) {
-        console.log(response);
-
+  const submit = () => {
+    axios.post('https://my-songs-backend.herokuapp.com/authentication/login/', value)
+      .then((response) => {
         localStorage.setItem('authToken', response.data.result.access_token);
         history.replace('/');
       })
@@ -34,8 +33,40 @@ const Login = () => {
   }
   return (
     <div>
-      <div>Login</div>
-      <Button onClick={clicca} label="setToken" primary />
+      <Box fill align="center" justify="center">
+        <Box width="medium">
+          <Form
+            value={value}
+            onChange={nextValue => {
+              console.log("Change", nextValue);
+              setValue(nextValue);
+            }}
+            onReset={() => setValue(defaultValue)}
+            onSubmit={event =>
+              console.log("Submit", event.value, event.touched)
+            }
+          >
+            <FormField label="Email" name="email" required>
+              <MaskedInput
+                name="email"
+                mask={[
+                  { regexp: /^[\w\-_.]+$/, placeholder: "example" },
+                  { fixed: "@" },
+                  { regexp: /^[\w]+$/, placeholder: "my" },
+                  { fixed: "." },
+                  { regexp: /^[\w]+$/, placeholder: "com" }
+                ]}
+              />
+            </FormField>
+            <FormField label="Password" name="password" type="password" required />
+            <Box direction="row" justify="between" margin={{ top: "medium" }}>
+
+              <Button type="reset" label="Reset" primary />
+              <Button type="submit" label="Log In" primary />
+            </Box>
+          </Form>
+        </Box>
+      </Box>
     </div>
   )
 };
