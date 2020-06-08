64b6cf97b39c717f2cbd8d15d4ca1915b7dfb782
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2020-06-08 18:44:21 +0200

    Added songs

--- src/components/Songs/songs.jsx
+++ src/components/Songs/songs.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Segment, Header, Icon, Grid, Button, Table, Dimmer, Loader } from 'semantic-ui-react';
+import { Segment, Header, Icon, Grid, Button, Table, Dimmer, Loader, Form, Modal, Rating, Divider } from 'semantic-ui-react';
 import RatingNew from '../RatingNew/ratingNew';
 import { loggedInstance } from '../../axiosConfig';
 
@@ -13,6 +13,19 @@ const Songs = () => {
   const [loadingData, setLoadingData] = useState();
 
 
+  const [open, setOpen] = useState();
+  const [confirmDelete, setConfirmDelete] = useState();
+
+  const [artist, setArtist] = useState();
+  const [title, setTitle] = useState();
+  const [difficulty, setDifficulty] = useState();
+  const [acoustic, setAcoustic] = useState();
+  const [dfm, setDfm] = useState();
+  const [backing, setBacking] = useState();
+  const [tab, setTab] = useState();
+  const [notes, setNotes] = useState();
+  const [id, setId] = useState();
+  const [loadingCrud, setLoadingCrud] = useState();
 
   const [column, setColumn] = useState();
   const [direction, setDirection] = useState();
@@ -20,7 +33,7 @@ const Songs = () => {
   useEffect(() => loadSongs(), [])
 
   const loadSongs = () => {
-    //setOpen(false);
+    setOpen(false);
     setLoadingData(true);
     loggedInstance.get('songs/')
       .then(response => {
@@ -32,6 +45,69 @@ const Songs = () => {
       .finally(() => setLoadingData(false));
   }
 
+  const add = () => {
+    setOpen(true);
+    setArtist('');
+    setTitle('');
+    setDifficulty("0");
+    setAcoustic(false);
+    setDfm(false);
+    setBacking(false);
+    setTab(false);
+    setNotes('');
+    setId('');
+  }
+
+  const update = (item) => {
+    setOpen(true);
+    setConfirmDelete(false);
+    setArtist(item.artist);
+    setTitle(item.title);
+    setDifficulty(item.difficulty);
+    setAcoustic(item.acoustic);
+    setDfm(item.dfm);
+    setBacking(item.backing);
+    setTab(item.tab);
+    setNotes(item.notes);
+    setId(item._id.$oid);
+  }
+
+  const onClose = () => setOpen(false);
+
+  const onChangeArtist = (event) => setArtist(event.target.value);
+  const onChangeTitle = (event) => setTitle(event.target.value);
+  const onChangeDifficulty = (e, { rating }) => setDifficulty(rating);
+  const onChangeAcoustic = (e, { checked }) => setAcoustic(checked);
+  const onChangeDfm = (e, { checked }) => setDfm(checked);
+  const onChangeBacking = (e, { checked }) => setBacking(checked);
+  const onChangeTab = (e, { checked }) => setTab(checked);
+  const onChangeNotes = (event) => setNotes(event.target.value);
+
+  const addUpdateSongs = () => {
+    setLoadingCrud(true);
+    if (id) {
+      loggedInstance.put('songs/' + id, { artist, title, difficulty, acoustic, dfm, backing, tab, notes })
+        .then(response => loadSongs())
+        .catch(error => console.log(error))
+        .finally(() => setLoadingCrud(false));
+    } else {
+      loggedInstance.post('songs/', { artist, title, difficulty, acoustic, dfm, backing, tab, notes })
+        .then(response => loadSongs())
+        .catch(error => console.log(error))
+        .finally(() => setLoadingCrud(false));
+    }
+  }
+
+  const onDelete = () => setConfirmDelete(true);
+
+  const deleteSong = () => {
+    setConfirmDelete(false);
+    setLoadingCrud(true);
+    loggedInstance.delete('songs/' + id)
+      .then(response => loadSongs())
+      .catch(error => console.log(error))
+      .finally(() => setLoadingCrud(false));
+  }
 
   const handleSort = (clickedColumn) => () => {
     if (column !== clickedColumn) {
@@ -62,7 +138,7 @@ const Songs = () => {
             <Button onClick={back} icon="arrow left" fluid content="Back Home"></Button>
           </Grid.Column>
           <Grid.Column>
-            <Button icon="plus" fluid color="olive" content="Add Song"></Button>
+            <Button onClick={add} icon="plus" fluid color="olive" content="Add Song"></Button>
           </Grid.Column>
         </Grid>
       </Segment>
@@ -85,7 +161,7 @@ const Songs = () => {
         <Table.Body>
           {tableData?.map((song, i) => {
             return (
-              <Table.Row key={i}>
+              <Table.Row key={i} onClick={() => update(song)}>
                 <Table.Cell>{song.artist}</Table.Cell>
                 <Table.Cell>{song.title}</Table.Cell>
                 <Table.Cell singleLine>
@@ -96,6 +172,36 @@ const Songs = () => {
           })}
         </Table.Body>
       </Table>
+
+      <Modal open={open}>
+        <Modal.Header>{id ? 'Update Song' : 'Add Song'}</Modal.Header>
+        <Modal.Content>
+          <Form onSubmit={addUpdateSongs} loading={loadingCrud}>
+            <Form.Input label='Artist' name="artist" value={artist} placeholder='Artist' onChange={onChangeArtist} required />
+            <Form.Input label='Title' name="title" value={title} placeholder='Title' onChange={onChangeTitle} required />
+            <Form.Field>
+              <label>Difficulty</label>
+              <Rating icon="star" size="huge" color="olive" defaultRating={difficulty} maxRating={5} onRate={onChangeDifficulty} clearable></Rating>
+            </Form.Field>
+            <Form.Checkbox label='Acoustic' checked={acoustic} onChange={onChangeAcoustic}></Form.Checkbox>
+            <Form.Checkbox label='Dont Forget Me!' checked={dfm} onChange={onChangeDfm}></Form.Checkbox>
+            <Form.Checkbox label='Backing Track' checked={backing} onChange={onChangeBacking}></Form.Checkbox>
+            <Form.Checkbox label='Tab' checked={tab} onChange={onChangeTab}></Form.Checkbox>
+            <Form.Input label='Notes' name="notes" value={notes} placeholder='Notes' onChange={onChangeNotes} />
+            <Divider horizontal style={{ margin: '25px' }}>Confirm</Divider>
+            {id ?
+              (<Button.Group fluid>
+                <Button type="submit" positive>Update</Button>
+                <Button.Or text='O' />
+                {!confirmDelete ? (<Button onClick={onDelete} negative>Delete</Button>) : ''}
+                {confirmDelete ? (<Button onClick={deleteSong} negative>Confirm Delete?</Button>) : ''}
+              </Button.Group>) :
+              (<Button type="submit" fluid positive>{id ? 'Update' : 'Add'}</Button>)}
+          </Form>
+          <Divider horizontal style={{ margin: '25px' }}>Close</Divider>
+          <Button onClick={onClose} fluid>Close</Button>
+        </Modal.Content>
+      </Modal>
     </>
   )
 }
